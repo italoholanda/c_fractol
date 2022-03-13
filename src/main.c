0243911cd14623d5135a5931b60e4483a16d3aa1
italo <you@example.com>
2022-03-13 18:45:38 -0300

    mouse handler

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: igomes-h <italogholanda@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 10:36:45 by igomes-h          #+#    #+#             */
-/*   Updated: 2022/03/12 16:23:14 by igomes-h         ###   ########.fr       */
+/*   Updated: 2022/03/13 18:45:10 by igomes-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,27 @@ int	check_args(char *arg)
 	return (1);
 }
 
+int	mouse_hook(int keycode, t_mlx *mlx)
+{
+	static int	count;
+
+	if (!count)
+		count = 0;
+	if (mlx)
+		printf("%i\n", keycode);
+	printf("%i", count);
+	printf("Hello from mouse_hook!\n");
+	count++;
+	return (0);
+}
+
+void	make_fractol_img(t_img *img, t_mlx *mlx, char *arg)
+{
+	mlx_set_img(mlx, img);
+	get_fractal(*img, arg);
+	mlx_put_image_to_window(mlx->ptr, mlx->win, img->img, 0, 0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_mlx	mlx;
@@ -33,8 +54,7 @@ int	main(int argc, char **argv)
 	if (check_args(argv[1]))
 		return (get_invalid_args_err());
 	mlx_set_win(&mlx);
-	mlx_set_img(&mlx, &img);
-	get_fractal(img, argv[1]);
-	mlx_put_image_to_window(mlx.ptr, mlx.win, img.img, 0, 0);
+	make_fractol_img(&img, &mlx, argv[1]);
+	mlx_mouse_hook(mlx.win, mouse_hook, &mlx);
 	mlx_loop(mlx.ptr);
 }
